6c7bdcf7aa3fb4efaa6cada11028619d2032f512
laylson lopes alencar <32991646+laylsonalencar@users.noreply.github.com>
2023-05-16 17:09:20 -0300

    commit começo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function soma(n1, n2) {
+    alert(n1 + n2)
+    
+}
+ function subtraçao(n1, n2) {
+    alert(n1 - n2)
+ }
+
+ function multiplicação(n1, n2) {
+    alert(n1 * n2)
+ }
+ function divisão(n1, n2) {
+    alert(n1 / n2)
+ }
+ function mostrarnome() {
+   let nome = prompt( " qual seu nome?")
+   alert("olá," + nome)
+   
+ }
